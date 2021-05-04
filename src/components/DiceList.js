45be7f609ebb6c317ba35f578214d0544a899ee0
Jacob Miller <jbmiller015@gmail.com>
2021-05-04 07:31:18 -0700

    V1.2 Refactored data sharing between components

--- src/components/DiceList.js
+++ src/components/DiceList.js
@@ -5,35 +5,11 @@ import Modifier from "./Modifier";
 const DiceList = ({setDisplay, maxValues}) => {
 
 
-    const [currValue, setCurrValue] = useState(null);
-    const [modifier, setModifier] = useState(null);
-    const [currMax, setCurrMax] = useState(null);
-
-
-    const onSearchSubmit = (val) => {
-        let result = roll(val, modifier);
-        setCurrValue(result);
-        setCurrMax(val);
-    }
-
-    useEffect(() => {
-        setDisplay(currValue, currMax);
-    }, [currValue, currMax, setDisplay])
-
-    const roll = (maxVal, modifier = 1) => {
-        let total = 0;
-        for (let i = 0; i < modifier; i++) {
-            total += (Math.floor(Math.random() * maxVal) + 1);
-        }
-        return total;
-    }
-
-
     const renderList = maxValues.map((maxVal) => {
         return (
             <div className="item" key={maxVal} style={{width: '300px', justifyContent: 'center'}}>
                 <div className="left floated content">
-                    <Modifier onSubmit={onSearchSubmit} maxValue={maxVal} setMod={setModifier}/>
+                    <Modifier onSubmit={setDisplay} maxValue={maxVal}/>
                 </div>
 
             </div>
--- src/components/Modifier.js
+++ src/components/Modifier.js
@@ -1,25 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 
 
-const Modifier = ({onSubmit, maxValue, setMod}) => {
+const Modifier = ({setDisplay, maxValue}) => {
 
 
     const [modifier, setModifier] = useState(1);
 
-    useEffect(() => {
-        setMod(modifier);
-
-    }, [modifier, setMod]);
 
     const onFormSubmit = (event => {
         event.preventDefault();
-        onSubmit(maxValue);
+        let currVal = roll(maxValue, modifier)
+        setDisplay(currVal, maxValue);
 
         //how to solve 'property 'state' of undefined...':
         //1. Function binding in constructor
         //2. Turn function into arrow function (arrow functions automatically bind function
         //3. Pass onSubmit arrow function that invokes onFormSubmit: <form className="ui form" onSubmit={(e) => this.onFormSubmit(e)}>
     })
+
+    const roll = (maxVal, modifier = 1) => {
+        let total = 0;
+        for (let i = 0; i < modifier; i++) {
+            total += (Math.floor(Math.random() * maxVal) + 1);
+        }
+        return total;
+    }
 
 
     return (<div>
